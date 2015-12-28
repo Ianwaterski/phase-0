02f6f7f12db5febf5329ada66c654b4e7f731c76
ianwudarski <eeinwud@gmail.com>
2015-12-28 18:00:54 -0500

    add ruby to my solution

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,15 @@
+# Format an Address
+
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+def make_address(street, city, state, zip)
+  puts street
+  puts city
+  puts state
+  puts zip.to_s
+end
+
+print make_address(Folsom St., San Francisco, CA, 94107)
