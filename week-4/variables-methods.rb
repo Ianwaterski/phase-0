d1f4eafa66bc68fba95a38c6e7c3ff66bf07e48d
ianwudarski <eeinwud@gmail.com>
2015-12-28 19:12:43 -0500

    add reflection to variables.rb

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,10 +1,35 @@
+``` ruby
 puts 'What is your favorite number?'
 number = gets.chomp
 
 puts number.to_i + 1
 puts 'is a bigger better number!'
-
+```
 
 # https://github.com/Ianwaterski/phase-0/blob/master/week-4/math/my_solution.rb <br>
 
 # https://github.com/Ianwaterski/phase-0/blob/master/week-4/address/my_solution.rb
+
+#How do you define a local variable?
+``` ruby
+local_variable = value
+```
+
+##How do you define a method?
+``` ruby
+def method_name(parameters)
+  what method does
+end
+```
+
+##What is the difference between a local variable and a method?
+#A local variable is only usable in the method that it's defined.
+
+##How do you run a ruby program from the command line?
+#ruby filename.rb
+
+##How do you run an RSpec file from the command line?
+#rspec filename.rb
+
+##What was confusing about this material? What made sense?
+#Most of it made sense.  I had some trouble with outputing the method in one exercise that was resolved by asking for help.  I was trying to output within the method instead of after when calling the method.
