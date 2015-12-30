933b3cabd7134b3782420960b0b3a9f8e7aa0d44
ianwudarski <eeinwud@gmail.com>
2015-12-30 14:44:15 -0500

    add ruby to solution

--- week-4/calculate-grade/my_solution.rb
+++ week-4/calculate-grade/my_solution.rb
@@ -1,6 +1,29 @@
 # Calculate a Grade
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Peter Stradakis].
 
 
 # Your Solution Below
+
+def get_grade(grade)
+
+
+  if grade >= 90
+    return "A"
+  elsif (grade <= 89 && grade >=80)
+    return "B"
+  elsif (grade <= 79 && grade >=70)
+    return "C"
+  elsif (grade < 69 && grade >=60)
+    return "D"
+  elsif grade < 60
+    return "F"
+  end
+
+
+
+end
+
+puts "What is your numerical grade?"
+grade = gets.chomp.to_i
+get_grade(grade)
