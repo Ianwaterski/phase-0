332ed161ac2f2381879223d3c6cc048785451474
ianwudarski <eeinwud@gmail.com>
2016-01-02 17:39:46 -0500

    add code to finish challene

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -4,33 +4,61 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Baron Kwan].
 
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
 
-# Input:
-# Output:
+# Input: Array of numbers
+# Output: Sum of the numbers in the array
 # Steps to solve the problem.
-
+# Use .each method to iterate through array and sum the numbers
 
 # 1. total initial solution
 
+#def total(numbers)
+#  sum_of_total = 0
+#  numbers.each do |x|
+#    sum_of_total = x + sum_of_total
+#  end
 
+#  return sum_of_total
+
+#end
 
 # 3. total refactored solution
 
+def total(numbers)
+  sum_of_total = 0
+  numbers.each do |x|
+    sum_of_total = x + sum_of_total
+  end
+  sum_of_total
+end
 
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
+# Input: Array of string
+# Output: Combined strings
 # Steps to solve the problem.
 
 
 # 5. sentence_maker initial solution
-
-
+#def sentence_maker(words)
+#  combined_strings = ""
+#  words.each do |x|
+#    combined_strings = combined_strings + " " + x.to_s
+#  end
+#  combined_strings.strip.capitalize + "."
+#end
 
 # 6. sentence_maker refactored solution
+
+def sentence_maker(words)
+  combined_strings = ""
+  words.each do |x|
+    combined_strings += " " + x.to_s
+  end
+  combined_strings.strip.capitalize + "."
+end
