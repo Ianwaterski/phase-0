b431016cac88a4c5736529d24d95238348296782
ianwudarski <eeinwud@gmail.com>
2016-01-02 16:47:33 -0500

    add changes to my_solution

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,7 +1,23 @@
 # Factorial
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Baron Kwan].
 
 
 # Your Solution Below
 def factorial(number)
+ # Your code goes here
+ total = 1
+
+     if number == 0
+         return total
+     end
+
+    while number >= 1
+         total *= number
+         number -= 1
+
+     end
+
+     return total
+
+end
