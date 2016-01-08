df907979338721437b8f5ead0fb3f4275990be13
ianwudarski <eeinwud@gmail.com>
2016-01-08 16:56:20 -0500

    add final solution

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,8 +1,8 @@
 # Calculate the mode Pairing Challenge
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge with Mike Pintar.
 
-# I spent [] hours on this challenge.
+# I spent [2.5] hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -12,19 +12,36 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? An Array of numbers of strings.
+# What is the output? An array of the most frequent values.
 # What are the steps needed to solve the problem?
+=begin
+
+Iterate through the array and count the instance of each object.
+-create a new Hash
+-add each object in the array as a key
+-add one to the value of the key if it repeats
+Identify the largest value or values.
+Create an array from the values of the most occurring object or objects.
+Return said array.
+
+=end
 
 
 # 1. Initial Solution
 
+def mode (array)
+  newhash=Hash.new
+  array.each do |x|
+    newhash[x] = newhash[x].to_i + 1
+  end
 
-
-
-# 3. Refactored Solution
-
-
-
-
-# 4. Reflection
+  modevalue = newhash.values.max
+  modearray= Array.new
+  newhash.each do |x, y|
+    if y == modevalue
+      modearray.push(x)
+    end
+  end
+  modearray
+end
