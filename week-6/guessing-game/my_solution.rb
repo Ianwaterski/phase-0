1bf1652b11a769832a376b6d385eecc6f092f640
ianwudarski <eeinwud@gmail.com>
2016-01-14 12:54:13 -0500

    add ruby to solution

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -1,25 +1,52 @@
 # Build a simple guessing game
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 # I spent [#] hours on this challenge.
 
 # Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: An integer
+# Output: A comparison as to whether or not the input is higher, lower, or correct based on a random integer.
+# Steps: Write a class that accepts an integer (guess) as an input.  It will then compare the input against a randomly generated number(answer).  IF the number is higher than the answer it will return Too high, if lower, it will return too low.  This will continue until the correct answer is reached.
 
 
 # Initial Solution
 
 class GuessingGame
+  attr_reader :answer
   def initialize(answer)
-    # Your initialization code goes here
+    @answer = answer
+    @solved = false
   end
 
-  # Make sure you define the other required methods, too
+  def guess(guess)
+    if guess.to_i > @answer
+      @solved = false
+      return :high
+    elsif guess.to_i < @answer
+      @solved = false
+      return :low
+    else
+      @solved = true
+      return :correct
+    end
+  end
+
+  def solved?
+    if @solved
+      @solved = true
+    else
+      @solved = false
+    end
+  end
+
 end
+
+
+# new = GuessingGame.new(10)
+# p new.guess(8)
+
 
 
 
