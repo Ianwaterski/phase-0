1754524b2c6d7da68f859ddd3a6ddcdf8ea39b1c
ianwudarski <eeinwud@gmail.com>
2016-01-16 20:09:44 -0500

    add refactored solution to file

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -1,34 +1,115 @@
 # Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge [with: Matthew Baquerizo].
+# I spent [2] hours on this challenge.
 
 # Pseudocode
 
-# Input:
-# Output:
-# Steps:
-
+# Input: credit card number (string)
+# Output: true/false (boolean)
+# Steps: create creditcard (class).  Going to accept a credit card number as argument.  Define check_card method.  Split the string to seperate numbers, then convert to integers. Iterate through array, only doubling indexes that evenly divisible by 2. Assign doubled values to new array. Convert new array to string, split string, convert to integer and add.  Check to see if number is divisible evenly by 10.  If it is return true.
 
 # Initial Solution
 
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
 
-class CreditCard
+# class CreditCard
+#   def initialize(numbers)
+#     @numbers = numbers
 
-end
+#     raise ArgumentError.new("#{@numbers} should be 16 digits. Your number was #{@numbers.length} digits") unless @numbers.length == 16
+
+#   end
+
+
+#   def check_card
+
+#     numbers_array = @numbers.split('')
+
+#     numbers_array.map! do |num|
+#       num.to_i
+#     end
+
+#     numbers_array.each_index do |i|
+#       if i % 2 == 0
+#         numbers_array[i] *= 2
+#       end
+#     end
+
+#     numbers_array.map! do |x|
+#       x.to_s.split('')
+#     end
+#     numbers_array.flatten!
+
+#     numbers_array.map! do |num|
+#       num.to_i
+#     end
+
+#     sum = numbers_array.inject{|sum, x| sum + x}
+#     if sum % 10 == 0
+#       return true
+#     else
+#       return false
+#     end
+
+#   end
+
+# end
+
+# creditcard = CreditCard.new("1234567891011109021")
+# p creditcard.check_card
 
 
 
 # Refactored Solution
 
+class CreditCard
+  def initialize(numbers)
+    @numbers = numbers
+
+    raise ArgumentError.new("#{@numbers} should be 16 digits. Your number was #{@numbers.to_s.length} digits") unless @numbers.to_s.length == 16
+
+  end
 
 
+  def check_card
+    numbers_string = @numbers.to_s
+
+    numbers_array = @numbers.to_s.split('').map! do |num|
+      num.to_i
+    end
+
+    numbers_array.each_index do |i|
+       numbers_array[i] *= 2 if i % 2 == 0
+    end
 
 
+    #Feels like we should be able to refactor, but not sure how
+    numbers_array.map! do |x|
+      x.to_s.split('')
+    end
+
+    numbers_array.flatten!.map! do |num|
+      num.to_i
+    end
+
+    sum = numbers_array.inject{|sum, x| sum + x}
+
+    sum % 10 == 0 ? true : false
 
 
+  end
+end
+
+
+# creditcard = CreditCard.new("1234567891011021")
+# p creditcard.check_card
 
 # Reflection
+# What was the most difficult part of this challenge for you and your pair?
+
+# What new methods did you find to help you when you refactored?
+
+# What concepts or learnings were you able to solidify in this challenge?
