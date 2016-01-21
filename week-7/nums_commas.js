0a9229d122f3602adb0ec9d564294744cca8033c
ianwudarski <eeinwud@gmail.com>
2016-01-21 13:37:59 -0500

    add intitial

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -1,14 +1,21 @@
 // Separate Numbers with Commas in JavaScript **Pairing Challenge**
 
 
-// I worked on this challenge with: .
+// I worked on this challenge with: Emmett Susselin.
 
 // Pseudocode
 
 
 
 // Initial Solution
+function reverse(s) {
+  var o = '';
+  for (var i = s.length - 1; i >= 0; i--)
+    o = o + s[i];
+  return o;
+}
 
+console.log(reverse(1000));
 
 
 
